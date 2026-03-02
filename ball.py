8329b32a0d69231a7f714fffea59171a32b332a2
carteryee7 <carter.yee07@gmail.com>
2026-03-02 01:57:42 -0500

    collisions

--- ball.py
+++ ball.py
@@ -1,16 +1,31 @@
 import pygame
 
 class Ball:
-    def __init__(self, x, y, radius, color, vel):
+    def __init__(self, x, y, radius, color, velo):
         self.x = x
         self.y = y
         self.radius = radius
         self.color = color
-        self.vel = vel
+        self.velo = velo
 
     def move(self):
-        self.x += self.vel[0]
-        self.y += self.vel[1]
+        self.x += self.velo[0]
+        self.y += self.velo[1]
+    
+    def scale_velo(self, vx, vy):
+        self.velo[0] *= vx
+        self.velo[1] *= vy
+    
+    def add_to_velo(self, vx, vy):
+        self.velo[0] += vx
+        self.velo[1] += vy
+    
+    def set_velo(self, vx, vy):
+        self.velo[0] = vx
+        self.velo[1] = vy
+    
+    def get_velo(self):
+        return self.velo
 
     def draw(self, surface):
         pygame.draw.circle(surface, self.color, (int(self.x), int(self.y)), self.radius)
--- display.py
+++ display.py
@@ -6,9 +6,43 @@ screen = pygame.display.set_mode((800, 600))
 clock = pygame.time.Clock()
 
 vel = [4, 0]
+vel2 = [4, 0]
 gravity = 0.3
-ball = Ball(400, 20, 20, (255, 255, 0), vel)
+b = Ball(400, 20, 20, (255, 255, 0), vel)
+b2 = Ball(200, 20, 20, (255, 0, 0), vel2)
 stopped = False
+
+def check_collision(ball: Ball):
+    # floor collision
+    if ball.y + ball.radius > 600:
+        ball.y = 600 - ball.radius
+        ball.scale_velo(1, -0.8)
+        if abs(ball.get_velo()[0]) < 0.5:
+            ball.set_velo(ball.get_velo()[0], 0)
+            stopped = True
+    
+    # ceiling collision
+    if ball.y - ball.radius <= 0:
+        ball.y = ball.radius
+        ball.scale_velo(1, -1)
+    
+    # wall collision
+    if ball.x + ball.radius >= 800:
+        ball.x = 800 - ball.radius
+        ball.scale_velo(-0.8, 1)
+    
+    # wall collision
+    if ball.x - ball.radius <= 0:
+        ball.x = ball.radius
+        ball.scale_velo(-0.8, 1)
+
+def check_ball_collision(b1: Ball, b2: Ball):
+    rad = max(b.radius, b2.radius)
+    if (abs(b1.x - b2.x) <= 2 * rad) and (abs(b.y - b2.y) <= 2 * rad):
+        b.velo[0] *= -1
+        b.velo[1] *= -1
+        b2.velo[0] *= -1
+        b2.velo[1] *= -1
 
 running = True
 while running:
@@ -19,35 +53,20 @@ while running:
     screen.fill((0, 0, 0))  # clear screen
     # draw things here
     
-    ball.move()
+    b.move()
+    b2.move()
 
-    # floor collision
-    if ball.y + ball.radius > 600:
-        ball.y = 600 - ball.radius
-        vel[1] *= -0.8
-        if abs(vel[1]) < 0.5:
-            vel[1] = 0
-            stopped = True
-    
-    # ceiling collision
-    if ball.y - ball.radius <= 0:
-        ball.y = ball.radius
-        vel[1] *= -1
-    
-    # wall collision
-    if ball.x + ball.radius >= 800:
-        ball.x = 800 - ball.radius
-        vel[0] *= -0.8
-    
-    # wall collision
-    if ball.x - ball.radius <= 0:
-        ball.x = ball.radius
-        vel[0] *= -0.8
+    check_collision(b)
+    check_collision(b2)
+
+    check_ball_collision(b, b2)
 
     if not stopped:
-        vel[1] += gravity
+        b.add_to_velo(0, gravity)
+        b2.add_to_velo(0, gravity)
 
-    ball.draw(screen)
+    b.draw(screen)
+    b2.draw(screen)
     pygame.display.flip()
     clock.tick(60)  # 60 FPS
 
